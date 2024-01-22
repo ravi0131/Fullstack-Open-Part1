3c4c1ff959681931dec6160427291a1a4fe522c5
ravi0131 <ravi.kumar@stud.tu-darmstadt.de>
2024-01-22 05:04:54 +0100

    Part 1c: page rerendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-/*
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-*/
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+let counter = 1
+
+const refresh = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <App counter={counter} />
+  )
+}
+
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
