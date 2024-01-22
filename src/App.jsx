8350361866073fa093413f97dc732328088c03df
ravi0131 <ravi.kumar@stud.tu-darmstadt.de>
2024-01-22 05:59:36 +0100

    1.c.7 re-rendering components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,50 @@
 import { useState } from 'react'
 
 const App = () => {
+  const [counter, setCounter] = useState(0)
 
-  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
 
-  const increaseByOne = () => setCounter(counter + 1)    
-  const setToZero = () => setCounter(0)
-  console.log('rendering....', counter)
+  const increaseByOne = () => {
+
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
-        <div>{counter}
-    <button onClick={increaseByOne }>
-        plus
-      </button>
-      <button onClick={setToZero}>
-                 zero
-      </button>   
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
-    
-  ) 
-}
+  )
+} 
 
+const Display = (props) => {
+  return (
+    <div>
+      {props.counter}
+    </div>
+  )
+}
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
 export default App
