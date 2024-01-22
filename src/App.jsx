ec6298bac7fd398946d0a01e120a8b791701ccca
ravi0131 <ravi.kumar@stud.tu-darmstadt.de>
2024-01-22 05:41:50 +0100

    event handlers

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ const App = () => {
 
   const [ counter, setCounter ] = useState(0)
 
-
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const increaseByOne = () => setCounter(counter + 1)    
+  const setToZero = () => setCounter(0)
   console.log('rendering....', counter)
 
   return (
-    <div>{counter}</div>
-  )
+        <div>{counter}
+    <button onClick={increaseByOne }>
+        plus
+      </button>
+      <button onClick={setToZero}>
+                 zero
+      </button>   
+    </div>
+    
+  ) 
 }
 
 export default App
